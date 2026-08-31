04a637124429e3017d0ded1f6728f20b1cd8afeb
AaliyaMohammedcyber <kk4786102@gmail.com>
2026-08-31 19:08:49 +0530

    Arithmetic operators exercise example

---
+++ arithemetic_operators_.py
@@ -0,0 +1,17 @@
+#Arithmetic operators 
+a=int(input('Enter Num1:'))
+b=int(input("Enter Num2:"))
+#addition operator
+print("addition of",a,"and",b,"is:",a+b)
+#multiplicatin operator
+print("multiplication of",a,"and",b,"is:",a*b)
+#subtraction operator
+print("subtraction of",a,"and",b,"is:",a-b)
+#division operator
+print("division of",a,"and",b,"is:",a/b)
+#floor divsion operator
+print("floor divsion of",a,"and",b,"is:",a//b)
+#division for remainder operator
+print("remainder of",a,"and",b,"is:",a%b)
+#exponent operator
+print("exponent of",a,"and",b,"is:",a**b)
