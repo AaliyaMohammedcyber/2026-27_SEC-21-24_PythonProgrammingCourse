cfc027ea58b9a97e390cf858ec2359d96387ee27
AaliyaMohammedcyber <kk4786102@gmail.com>
2026-08-31 19:13:36 +0530

    Comparison operators exercise example

---
+++ comparision_operators_exercise.py
@@ -0,0 +1,15 @@
+# Python Comparison Operators Exercise
+a = 10
+b = 20
+# Equal to
+print("result of ",a," == ",b,":", a == b) 
+# Not equal to
+print("result of ",a," != ",b,":", a != b)  
+# Greater than
+print("result of ",a," > ",b,":", a > b)   
+# Less than
+print("result of ",a," < ",b,":", a < b)   
+# Greater than or equal to
+print("result of ",a," >= ",b,":", a >= b) 
+# Less than or equal to
+print("result of ",a," <= ",b,":", a <= b)
