c19a462f86877bb34e4679d67380bbc893a8fc74
AaliyaMohammedcyber <kk4786102@gmail.com>
2026-09-04 14:28:23 +0530

    Identity operator exercise example

---
+++ identity_operator_exercise.py
@@ -0,0 +1,15 @@
+#IDENTITY OPERATOR
+#IS operator
+a=[3,4,5,7]
+b=a
+result=a is b
+answer1=a is not b
+print("the output of ",a,"is",b,":",result)
+print("the output of ",a,"is not",b,":",answer1)
+#IS not operator
+c=[0,9,8,7,6]
+d=[0,9,8,7,6]
+result1=c is d
+answer=c is not d
+print("the output of ",c,"is",d,":",result1)
+print("the output of ",c,"is not",d,":",answer)
