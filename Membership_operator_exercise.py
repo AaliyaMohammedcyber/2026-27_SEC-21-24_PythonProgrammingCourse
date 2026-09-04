2b1720e9b7ece0ee0d8a5a2b3988f5a90453706f
AaliyaMohammedcyber <kk4786102@gmail.com>
2026-09-04 14:40:13 +0530

    Membership operator exercise example

---
+++ Membership_operator_exercise.py
@@ -0,0 +1,28 @@
+a=[1,2,3,4]
+b='student'
+c={2,5,6,7,0,0}
+d={'name':"human",'age':36,'place':"viyayawada"}
+e=(4,5,6,'rat')
+result='a' in b
+answer='3' in a
+output='5' in c
+solution='place' in d
+teju='r' in e
+# in operator
+print("a in",b,":",result)
+print("3 in",a,":",answer)
+print("5 in",c,":",output)
+print("place in",d,":",solution)
+print("r in",e,":",teju)
+# not in
+result1='d'not in b
+answer1='2'not in a
+output1='1'not in c
+solution1='name'not in d
+teju1='4' not in e
+# in operator
+print("d  not in",b,":",result1)
+print("2 not in",a,":",answer1)
+print("1 not in",c,":",output1)
+print("name not in",d,":",solution1)
+print("4 not in",e,":",teju1)
