f634ac6426c839c8884d3f8b5248819db83147ac
AaliyaMohammedcyber <kk4786102@gmail.com>
2026-09-10 20:16:30 +0530

    calculate the simple interest

---
+++ Unit-1_Task-6_simple_intrest.py
@@ -0,0 +1,6 @@
+#AIM:calculate simple intrest.
+principal=float(input("ENTER THE PRINCIPAL AMOUNT (IN ₹):"))
+rate_of_intrest=float(input("ENTER THE RATE OF INTREST (IN % PER YEAR):"))
+time=float(input('ENTER THE TIME (IN YEARS):'))
+simple_intrest=(principal*rate_of_intrest*time)/100
+print(simple_intrest)
