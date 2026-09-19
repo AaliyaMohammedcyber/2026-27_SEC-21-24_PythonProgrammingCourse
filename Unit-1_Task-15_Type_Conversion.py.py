d079d3d2999b251d25ae47944aa08bdabad5fd06
AaliyaMohammedcyber <kk4786102@gmail.com>
2026-09-19 22:58:26 +0530

    Type conversion exercise

---
+++ Unit-1_Task-15_Type_Conversion.py.py
@@ -0,0 +1,32 @@
+
+#TYPE CONVERSIONS
+#INTEGER TO FLOAT
+int_num=25
+float_num=float(int_num)
+print(float_num)
+print(type(float_num))
+#INTEGER TO STRING
+int_num=56
+str_num=str(int_num)
+print(str_num)
+print(type(str_num))
+#float to integer
+float_num=25
+int_num=int(float_num)
+print(int_num)
+print(type(int_num))
+#float to string
+float_num=25
+str_num=str(float_num)
+print(str_num)
+print(type(str_num))
+#string to float
+str_num=77
+float_num=float(str_num)
+print(float_num)
+print(type(float_num))
+#string to integer
+str_num=77
+int_num=int(str_num)
+print(int_num)
+print(type(int_num))
